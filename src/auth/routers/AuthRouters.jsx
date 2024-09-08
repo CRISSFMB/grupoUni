877ac3b0e24d369645ce81d27c3103436656f10c
cristian mosquera <cmosqbone1@gmail.com>
2024-09-08 05:30:28 +0200

    loginadmin

---
+++ src/auth/routers/AuthRouters.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AuthRouters = () => {
+  return (
+    <Routes>
+      <Route path="/HomeAdmin" element={<FormDiscapacidad />} />
+    </Routes>
+  );
+};
+
+export default AuthRouters;
