f65b885bf59028eb16502e7c3a8514460c5d46ba
cristian mosquera <cmosqbone1@gmail.com>
2024-09-10 01:20:37 +0200

    agregando

--- src/auth/pages/loginAdminPage/LoginAdminPage.jsx
+++ src/auth/pages/loginAdminPage/LoginAdminPage.jsx
@@ -1,21 +1,46 @@
 import { Navigate, useNavigate } from "react-router-dom";
 import "../css/LoginAdmin.css";
 
+import React, { useState } from "react";
+
 const LoginAdminPage = () => {
-  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
   const handleSubmitLogin = (e) => {
     e.preventDefault();
 
+    if (email !== "istecgrupo@gmail.com" || password !== "istecgrupo") {
+      setError("Credenciales incorrectas. Intenta de nuevo.");
+      return;
+    }
+
+    setError("");
+
     window.open("https://juansolis.netlify.app/", "_blank");
   };
+
   return (
     <div className="login">
-      <h1>LOGINADMIN</h1>
+      <h1>ISTECADMIN</h1>
       <form onSubmit={handleSubmitLogin}>
-        <input type="email" />
-        <input type="password" />
-        <button type="submit">login</button>
+        <input
+          type="email"
+          placeholder="nombre admin"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="password admin"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Login</button>
       </form>
+
+      {error && <p style={{ color: "red" }}>{error}</p>}
     </div>
   );
 };
